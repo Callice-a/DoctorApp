dfd749a2226af11acd3daa2b34ec3009ea760b5b
Trevor von Hake <tvonhake@outlook.com>
2021-03-18 14:45:24 -0600

    seeds created and working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+  Physician.create(name: Faker::Superhero.name)
+end
+
+5.times do
+  patient = Patient.create(name: Faker::Name.name)
+
+  6.times do |i|
+    Appointment.create(patient_id: patient.id, physician_id: i, appointment_date: Faker::Date.in_date_period)
+  end
+
+end
+
+puts "PHYSICIANS SIZE: #{Physician.all.length}"
+puts "PATIENT SIZE: #{Patient.all.length}"
+puts "APPOINTMENTS SIZE: #{Appointment.all.length}"
+
+
