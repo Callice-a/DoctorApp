8d54eea06b3bd801e599ec9346a062a93b15cf33
Trevor von Hake <tvonhake@outlook.com>
2021-03-18 17:23:42 -0600

    got patientshow.js rendering cards

--- client/src/pages/PatientShow.js
+++ client/src/pages/PatientShow.js
@@ -3,35 +3,37 @@ import AxiosContainer from '../components/AxiosContainer'
 import StringifyJson from '../components/StringifyJSON.js'
 import {useParams} from 'react-router-dom'
 import CardContainer from '../components/CardContainer'
+import Card from '../components/Card'
 
 const PatientShow = (props)=>{
  const {id} = useParams()
  const {data, loading, error} = useAxiosOnMount(`/api/patients/${id}`)
-//  console.log(data.physicians)
 
   // trying to make physician/appointment data look nicer
 
-  // const renderPhysicians = () => {
-  //   return data.physicians.forEach(object => {
-  //     console.log(object.physician)
-  //     return(
-  //       <>
-  //       <h2>Appointment</h2>
-  //       <h3>Physician: {object.physician.name}</h3>
-  //       {/* <h4>Appointment date: {physician.appointment}</h4> */}
-  //       </>
-  //     )}
+  const renderPhysicians = () => {
+    return data.physicians.map(object => {
+      return(
+        <>
+        <Card>
+        <h2>Appointment {object.physician.id}</h2>
+        <h3>Physician: {object.physician.name}</h3>
+        <h4>Appointment date: {object.appointment}</h4>
+        <br/>
+        </Card>
+        </>
+      )}
 
-  //   )
-  // }
+    )
+  }
 
 
  return (
    <>
+   {data && <h1>Appointments for: {data.patient.name}</h1>}
      <AxiosContainer fullError loading={loading} error={error} loaderMessage={'Loading, please wait'}>
         <CardContainer>
-          <StringifyJson json={data} />
-          {/* {renderPhysicians} */}
+          {data && renderPhysicians()}
         </CardContainer>
      </AxiosContainer>
      </>
