0631e877a90c1545e7755b315ebc53aae9075c0a
Trevor von Hake <tvonhake@outlook.com>
2021-03-19 17:06:17 -0600

    Patient New Form working

--- client/src/pages/Patients.js
+++ client/src/pages/Patients.js
@@ -9,11 +9,16 @@ import Button from '../components/Button'
 import axios from 'axios'
 import {useHistory} from 'react-router-dom'
 import {useState, useEffect} from 'react'
+import PatientsEdit from '../components/PatientsEdit'
+import PatientsNew from '../components/PatientsNew'
 
 const Patients = (props)=>{
   const {data, loading, error} = useAxiosOnMount('/api/patients') 
   let history = useHistory()
+  const [show, setShow] = useState(false)
   const [patients, setPatients] = useState([])
+  const [showForm, setShowForm] = useState(false)
+  const [showEditForm, setShowEditForm] = useState(false)
 
   useEffect(()=>{
     getPatients()
@@ -45,7 +50,9 @@ const Patients = (props)=>{
                   <Link to={`/patients/${patient.id}`}>
                          <h1>{patient.name}</h1>
                   </Link>
-                  <SEMButton>Edit</SEMButton>
+                  <SEMButton color='violet' onClick={()=> {setShowEditForm(!showEditForm)}}>{showEditForm ? 'Hide Form' : 'Edit'}</SEMButton>
+                  {showEditForm && <PatientsEdit getPatients={getPatients} setShowEditForm={setShowEditForm}/>}
+
                   <SEMButton onClick={()=>deletePatient(patient.id)} color='red'>Delete</SEMButton>
                 </Card>
               )} 
@@ -55,11 +62,17 @@ const Patients = (props)=>{
   }
 
   return (
+    <div>
+      {data && <h1>Patients</h1>}
+        <Button onClick={() => setShow(!show)}>{show ? 'Hide Form' : 'Add New Patient'}</Button>
+        {show && <PatientsNew getPatients={getPatients} setShowForm={setShowForm}/>}
+
      <AxiosContainer fullError loading={loading} error={error} loaderMessage={'Loading, please wait'}>
        <CardContainer>
           {renderPatients()}
         </CardContainer>
      </AxiosContainer>
+     </div>
   )
 }
 
