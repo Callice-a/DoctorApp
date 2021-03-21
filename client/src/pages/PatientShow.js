6b3a39b01238e64cc2c7da062b1485a83c2db1ed
Jedediah Wood <jedediahwood28@gmail.com>
2021-03-21 17:10:25 -0600

    pull merge push

--- client/src/pages/PatientShow.js
+++ client/src/pages/PatientShow.js
@@ -1,7 +1,6 @@
 import useAxiosOnMount from '../hooks/useAxiosOnMount'
 import AxiosContainer from '../components/AxiosContainer'
 import {useParams} from 'react-router'
-import CardContainer from '../components/CardContainer'
 import Card from '../components/Card'
 import CardContainer from '../components/CardContainer'
 import LoaderAndError from '../components/LoaderAndError'
